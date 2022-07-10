5f80e6b3dcf3f82fe123b2f12059260c1ee7c4ee
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-07-09 21:50:51 -0400

    Add bullets & rudimentary bullet behavior

---
+++ Assets/BulletBehavior.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+    }
+}
--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public float speed; //public: things outside PlayerBehavior will access the variable, access in inspector
+                        //inspector always OVERRIDES values 
+                        //so DELCARE here, then TWEAK in inspector
+
+    public GameObject bulletPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +20,21 @@ public class PlayerBehavior : MonoBehaviour
     void Update()
     {
 
-        Debug.Log(Input.GetAxis("Vertical"));
+        //Debug.Log(Input.GetAxis("Vertical"));
 
-        float speed = 10;
+        //MOVEMENT: WASD
         float maxDistanceToMove = speed * Time.deltaTime; //time difference
-        //move with W & S
-        transform.position += Vector3.forward * Input.GetAxis("Vertical") * maxDistanceToMove;
-        //move with A & D
-        transform.position += Vector3.right * Input.GetAxis("Horizontal") * maxDistanceToMove;
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical") ); //x, y, z
+        Vector3 movementVector = inputVector * maxDistanceToMove; //scale up pieces of inputVector by maxDistanceToMove
+        Vector3 newPosition = transform.position + movementVector;
 
+        transform.LookAt(newPosition);//change facing rotation
+        transform.position = newPosition;
+      
+        //click to fire
+        //if clicked, create bullet at our current position
+        if (Input.GetButton("Fire1")) {
+            Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
     }
 }
